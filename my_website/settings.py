59a234801420e25390dd3bb6302b82548d00b85b
runhao <runhao.hu@funenc.com>
2024-10-14 16:55:55 +0800

    feat: 修改主页

--- my_website/settings.py
+++ my_website/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, STATIC_URL),
+    os.path.join(BASE_DIR, 'home/static'),
 ]
 
 # Default primary key field type
