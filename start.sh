997f30a24a239ec4e8df64bfc4f867ea67d66071
runhao <runhao.hu@funenc.com>
2024-10-14 11:18:17 +0800

    feat: 修改nginx配置

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # 1. 生成数据库迁移文件
 # 2. 根据数据库迁移文件来修改数据库
 # 3. 用 uwsgi启动 django 服务, 不再使用python manage.py runserver
+nginx -c /etc/nginx/nginx.conf
 python manage.py makemigrations&&
 python manage.py migrate&&
 uwsgi --ini /var/www/html/my_website/uwsgi.ini
