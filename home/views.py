d8b459545c8158c9b7f4098b448cf9221249d46e
runhao <runhao.hu@funenc.com>
2024-10-15 16:54:05 +0800

    feat: 修改后端数字格式

--- home/views.py
+++ home/views.py
@@ -6,8 +6,18 @@ from django.http import HttpResponse
 
 
 def home(request):
+    def format_number(number):
+        """
+        格式转换
+        :param number:
+        :return:
+        """
+        number = str(int(number))
+        if int(number) < 10:
+            number = "0" + number
+        return number
     start_time = datetime.strptime("2024-07-06 16:00:00", "%Y-%m-%d %H:%M:%S")
     differ_time = datetime.now() - start_time
-    context = {"days": differ_time.days, "hour": int(differ_time.seconds/3600),
-               "minute": int((differ_time.seconds % 3600)/60), "second": int(differ_time.seconds % 60)}
+    context = {"days": differ_time.days, "hour": format_number(differ_time.seconds/3600),
+               "minute": format_number((differ_time.seconds % 3600)/60), "second": format_number(differ_time.seconds % 60)}
     return render(request, "home.html", context)
